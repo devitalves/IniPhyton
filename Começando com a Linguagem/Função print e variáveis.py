bb02e5030e5013d72d739dee2687d9d5bfb33dd3
Ítalo Alves <101436057+devitalves@users.noreply.github.com>
2022-12-21 20:43:20 -0300

    Função print e variáveis.

---
+++ Começando com a Linguagem/Função print e variáveis.py
@@ -0,0 +1,24 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:57:54) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print (Olá mundo
+       
+SyntaxError: invalid syntax
+>>> print ("Olá mundo!")
+Olá mundo!
+>>> print ("Brasil ganhou 5 titulos Mundiais")
+Brasil ganhou 5 titulos Mundiais
+>>> print ("Brasil", "ganhou", 5, "titulos Mundiais", sep="-")
+Brasil-ganhou-5-titulos Mundiais
+>>> print ("Brasil", "ganhou", 5, "titulos Mundiais", end="/n")
+Brasil ganhou 5 titulos Mundiais/n
+>>> 
+>>> 
+>>> pais = "Italia"
+>>> type(pais)
+<class 'str'>
+>>> quantidade = 4
+>>> type(quantidade)
+<class 'int'>
+>>> print(pais,"ganhou",quantidade, "titulos mundiais")
+Italia ganhou 4 titulos mundiais
+>>> 
