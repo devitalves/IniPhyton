e9ed94169b94e3a0694b91408536c1d6123ba85c
Ítalo Alves <101436057+devitalves@users.noreply.github.com>
2023-01-06 23:03:07 -0300

    Atualizar o Gerando números aleatório.py

--- Começando com a Linguagem/Gerando números aleatório.py
+++ Começando com a Linguagem/Gerando números aleatório.py
@@ -1,7 +1,7 @@
 import random
 
 print("*********************************")
-print("Bem vindo ao jogo de Adivinhação!")
+print("Bem vindo ao jogo de Adivinhação!!")
 print("*********************************")
 
 numero_secreto = random.randrange(1,101)
